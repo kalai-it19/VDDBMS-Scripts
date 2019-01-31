47c1906160be202008c06cc5a2f0a8f0bc56f84e
Shunmugam V <sanjayshanmu7@gmail.com>
2019-01-31 16:02:35 +0530

    Add files via upload

---
+++ mysql/add_customer_tbl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customer (
+  id int(11) NOT NULL auto_increment,
+  oid varchar(500) default NULL,
+  first_name varchar(255) default NULL,
+  last_name varchar(255) default NULL,
+  email varchar(255) default NULL,
+  PRIMARY KEY  (id)
+);
