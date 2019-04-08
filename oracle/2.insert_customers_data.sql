af5a9277eada17a79fe7b34f5ae1cdcd0f797356
kalai-it19 <46036271+kalai-it19@users.noreply.github.com>
2019-04-08 11:22:19 -0700

    Update 2.insert_customers_data.sql

--- oracle/2.insert_customers_data.sql
+++ oracle/2.insert_customers_data.sql
@@ -1,4 +1,2 @@
-INSERT ALL
-  INTO Customers1 (user_id , user_name, city, state1) VALUES (1, 'Tom B. Erichsen', 'Bellevue', 'Washington')
-  INTO Customers1 (user_id , user_name, city, state1) VALUES (2, 'Kalaiselvi', 'Bothell', 'Washington')
-SELECT * FROM dual;
+INSERT INTO Customers1 (user_id , user_name, city, state1) VALUES (1, 'Tom B. Erichsen', 'Bellevue', 'Washington');
+INSERT INTO Customers1 (user_id , user_name, city, state1) VALUES (2, 'Kalaiselvi', 'Bothell', 'Washington');
